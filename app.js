ce667e69c76476c412621ba1a78749aac2e09cad
Rajireddi <59596858+Rajireddi@users.noreply.github.com>
2021-01-06 00:24:26 +0530

    updated loan calc changes

--- app.js
+++ app.js
@@ -1,114 +1,42 @@
-const form = document.querySelector("#task-form");
-const taskList = document.querySelector(".collection");
-const clearBtn = document.querySelector(".clear-tasks");
-const filter = document.querySelector("#filter");
-const taskInput = document.querySelector("#task");
+document
+  .getElementById("loan-form")
+  .addEventListener("submit", calculateResult);
 
-loadEventListeners();
+function calculateResult(e) {
+  const amount = document.getElementById("amount");
+  const interest = document.getElementById("interest");
+  const years = document.getElementById("years");
+  const monthlyPayment = document.getElementById("monthly-payment");
+  const totalPayment = document.getElementById("total-payment");
+  const totalInterest = document.getElementById("total-intrest");
 
-function loadEventListeners() {
-  //   document.addEventListener("DOMContentLoaded", getTasks);
-  form.addEventListener("submit", addTask);
-  taskList.addEventListener("click", removeTask);
-  clearBtn.addEventListener("click", clearTasks);
-  filter.addEventListener("keyup", filterTasks);
-}
+  const principal = parseFloat(amount.value);
+  const calculatedinterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
 
-function getTasks() {
-  let tasks;
-  if (localStorage.getItem("tasks") === "null") {
-    tasks = [];
+  const x = Math.pow(1 + calculatedinterest, calculatedPayments);
+  const monthly = (principal * x * calculatedinterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
   } else {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
+    showError("Please Check Your Number");
   }
-  tasks.forEach(function (task) {
-    const li = document.createElement("li");
-    li.className = "collection-item";
-    li.appendChild(document.createTextNode(task));
-
-    const link = document.createElement("a");
-    link.className = "delete-item secondary-content";
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-    li.appendChild(link);
-    taskList.appendChild(li);
-  });
-}
-
-function addTask(e) {
-  if (taskInput.value === "") {
-    alert("Add a task");
-  }
-  const li = document.createElement("li");
-  li.className = "collection-item";
-  li.appendChild(document.createTextNode(taskInput.value));
-
-  const link = document.createElement("a");
-  link.className = "delete-item secondary-content";
-  link.innerHTML = '<i class="fa fa-remove"></i>';
-  li.appendChild(link);
-  taskList.appendChild(li);
-  storeTaskInLocalStorage(taskInput.value);
-  taskInput.value = " ";
   e.preventDefault();
 }
 
-function storeTaskInLocalStorage(task) {
-  let tasks;
-  if (localStorage.getItem("tasks") === "null") {
-    tasks = [];
-  } else {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-  }
-  tasks.push(task);
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+function showError(error) {
+  const errorDiv = document.createElement("div");
+  const card = document.querySelector(".card");
+  const heading = document.querySelector(".heading");
+  errorDiv.className = "alert alert-danger";
+  errorDiv.appendChild(document.createTextNode(error));
+  card.insertBefore(errorDiv, heading);
+  setTimeout(clearError, 3000);
 }
 
-function removeTask(e) {
-  if (e.target.parentElement.classList.contains("delete-item")) {
-    if (confirm("Are you sure?")) {
-      e.target.parentElement.parentElement.remove();
-      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
-    }
-  }
-}
-
-function removeTaskFromLocalStorage() {
-  let tasks;
-  if (localStorage.getItem(tasks) === "null") {
-    tasks = [];
-  } else {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-  }
-
-  tasks.forEach(function (task, index) {
-    if (taskItem.textContent === task) {
-      tasks.splice(index, 1);
-    }
-  });
-
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-}
-
-function clearTasks() {
-  while (taskList.firstChild) {
-    taskList.removeChild(taskList.firstChild);
-  }
-  //   taskList.innerHTML = "";
-  clearTaskFromLocalStorage();
-}
-
-function clearTaskFromLocalStorage() {
-  localStorage.clear();
-}
-
-function filterTasks(e) {
-  const text = e.target.value;
-  document.querySelectorAll(".collection-item").forEach(function (task) {
-    const item = task.firstChild.textContent;
-    if (item.toLowerCase().indexOf(text) != -1) {
-      task.style.display = "block";
-    } else {
-      task.style.display = "none";
-    }
-  });
+function clearError() {
+  document.querySelector(".alert").remove();
 }
