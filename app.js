301533fb9b3d9fb83c3493621f5d4dd3e189b0fb
Rajireddi <59596858+Rajireddi@users.noreply.github.com>
2021-01-05 12:02:45 +0530

    changes done

---
+++ app.js
@@ -0,0 +1,26 @@
+// data types
+//1.primitive
+//2.reference data type
+
+// primitive:
+// --------------------------
+// data is stored directly on the location.
+
+// types:
+// ---------
+// String
+// Number
+// Boolean
+// undefined
+// Symbols
+// null
+
+// reference type:
+// -------------------
+// accessed by reference,a pointer to a location in the memory.
+// types:
+// --------
+// arrays
+// objects
+// Dates
+// functions
